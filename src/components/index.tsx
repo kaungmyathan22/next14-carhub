4bf856066a17c3e43dfea2022c41328a947e16f3
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-31 14:32:21 +0630

    update(componets): default index exports added

---
+++ src/components/index.tsx
@@ -0,0 +1,6 @@
+import CustomButton from "./CustomButton/CustomButton";
+import Footer from "./Footer/Footer";
+import Hero from "./Hero/Hero";
+import Navbar from "./Navbar/Navbar";
+
+export { CustomButton, Footer, Hero, Navbar };

2da1386f4b00f65aa8189b088f2503484d91b953
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-31 15:09:13 +0630

    update(): index export updated

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,17 @@
 import CustomButton from "./CustomButton/CustomButton";
+import CustomFilter from "./CustomFilter/CustomFilter";
 import Footer from "./Footer/Footer";
 import Hero from "./Hero/Hero";
 import Navbar from "./Navbar/Navbar";
+import SearchManufacturer from "./SearchManufacturer/SearchManufacturer";
+import Searchbar from "./Searchbar/Searchbar";
 
-export { CustomButton, Footer, Hero, Navbar };
+export {
+  CustomButton,
+  CustomFilter,
+  Footer,
+  Hero,
+  Navbar,
+  SearchManufacturer,
+  Searchbar,
+};
